4b654e9270d99e5ee7ec0b466ddea462879c85f5
Sujeet Kulkarni <sujeet27kulk@gmail.com>
2026-05-06 00:12:56 -0400

    Sandbox name change.

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -176,7 +176,7 @@ export default function Footer() {
               </div>
               <div>
                 <a href={GLYGEN_SANDBOX} target="_blank" rel="noopener noreferrer">
-                  GlyGen&nbsp;SandBox
+                  GlyGen&nbsp;Sandbox
                 </a>
               </div>
               <div>
