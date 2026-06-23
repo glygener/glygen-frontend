b0c091d4e75b2dbef7fd5e3d30d2676a17241902
Sujeet Kulkarni <sujeet27kulk@gmail.com>
2026-06-23 16:06:42 -0400

    CFDE name change.

--- src/components/cards/MainFeaturedCard.js
+++ src/components/cards/MainFeaturedCard.js
@@ -178,7 +178,7 @@ export default function MainFeaturedCard(props) {
                       paddingLeft: "15px",
                     }}
                 >
-                CFDE Wheel
+                CFDE
                 </span>
               </div>
             </div>}
